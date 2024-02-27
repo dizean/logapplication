feb2539aabb35712689b682a03d2af7766b0ca4f
Poorsaken <chiyokogaming02@gmail.com>
2024-02-27 17:51:28 +0800

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -19,6 +19,8 @@ import ViewLogVisitors from './components/finalpages/ViewVisitorslog';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
+
+  {/* test ni jal */}
   <UserProvider>
   <React.StrictMode>
     <Router>
