750d11e44bd5aaf12063416e5096017dfd813c6d
ean <torrescharles0307@gmail.com>
2024-02-28 15:22:12 +0800

    update

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
-const port = 3002;
+const port = 3003;
 
 app.use(cors());
 app.use(bodyParser.json());
